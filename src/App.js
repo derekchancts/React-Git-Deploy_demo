3bbfe106e2f6b57d918c19e4e049f415f33cda29
derekchancts-at-878661647005 <derekchancts@yahoo.com.hk>
2021-02-28 18:58:46 +0800

    deploy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import BarChart from './components/BarChart';
+import LineChart from './components/LineChart';
+
+const App = () => (
+  <>
+    <BarChart />
+    <LineChart />
+  </>
+)
+  
+
+export default App;
